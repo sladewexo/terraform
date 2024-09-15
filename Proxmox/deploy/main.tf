f9832a414d12169a2a0b24ab5e3a2773ab49b923
Eric De Pessemier <43291788+sladewexo@users.noreply.github.com>
2024-09-15 18:00:06 +0200

    Update main.tf

--- Proxmox/deploy/main.tf
+++ Proxmox/deploy/main.tf
@@ -35,19 +35,21 @@ resource "proxmox_vm_qemu" "prepprovision-test" {
   memory = 2048
   scsihw = "virtio-scsi-single"
   bootdisk = "scsi0"
-  disk {
-    slot = "virtio0"
-    # set disk size here.
-    size = "32G"
-    type = "disk"
-    storage = "wexam-ceph-pool" # name of your proxmox storage
-    iothread = false
-  }
-    disks {
-        ide {
+  disks {
+       ide {
             ide3 {
                 cloudinit {
                     storage = "local-lvm"
+                }
+            }
+        }
+        virtio {
+            virtio0 {
+                disk {
+                    size = "32G"
+                    type = "disk"
+                    storage = "wexam-ceph-pool" # name of your proxmox storage
+                    iothread = false
                 }
             }
         }
