5073e1b26d0b59f8266d3fa20e8010d8bacd84e4
Eric De Pessemier <43291788+sladewexo@users.noreply.github.com>
2024-09-15 20:59:47 +0200

    Delete Proxmox/deploy directory

--- Proxmox/deploy/vars.tf
+++
@@ -1,9 +0,0 @@
-variable "ssh_key" {
-  default = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDD9vMkB6UiEud2BnnuEOxB8TTqdNBTF4ZYS1BGxjnWa ericdp@Erics-Laptop.local"
-}
-variable "proxmox_host" {
-    default = "pve01"
-}
-variable "template_name" {
-    default = "ubuntu-cloudinit"
-}
